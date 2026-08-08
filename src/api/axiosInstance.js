b4e24e89f0f6f67334bd7a899eb337114adea869
nivakclt <nivakclt@gmail.com>
2026-08-08 21:04:26 +0530

    connect frontend to deployed backend

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:3000",
-    baseURL:'https://resume-builder-react-fe-1.onrender.com',
+    baseURL:"https://resume-builder-react-be.onrender.com",
     timeout: 5000
 })
 
